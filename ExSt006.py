ac53341cc373c08df8ad6010f4b52956aa531df8
Lucas Oliveira <lucasoliveiraea@hotmail.com>
2022-08-05 13:15:54 -0300

    006

---
+++ ExSt006.py
@@ -0,0 +1,23 @@
+# Algoritimo Tarefa 005: Receber nomes e salários (While)
+# Dev: Lucas Oliveira
+# Data: 05.08.2022
+
+RendaPoupanca = 1.02
+RendaFundo = 1.05
+SalarioCarlos = float(input('Digite o salário do funcionário: '))
+SalarioJoao = (SalarioCarlos/3)
+
+print('Salario Carlos', SalarioCarlos)
+print('Salario Joao', SalarioJoao)
+
+Meses = 0
+while SalarioJoao < SalarioCarlos:
+    SalarioCarlos *= RendaPoupanca
+    SalarioJoao *= RendaFundo
+    Meses += 1
+
+if SalarioJoao > SalarioCarlos:
+    print("Para ultrapassar o valor pertencente à João levou:", Meses, "meses")
+elif SalarioJoao == SalarioCarlos:
+    print("Para igualar o valor pertencente à Carlos João levou:", Meses, "meses")
+
